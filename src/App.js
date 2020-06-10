a51c448152281a3ec99a26b867f32ad580fe3cb4
timeonator <timeonator@gmail.com>
2020-06-09 19:02:42 -0700

    moved load json from property to state in D3Bar component

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import './App.css';
 import D3Bar from './D3Bar'
 
 class App extends Component {
-   render() {
-   return (
-      <div className='App'>
-
-      <div>
-      <
-          D3Bar data={[10,9,8,7,6,5,4,3]} size={[500,500]} />
-      </div>
-      </div>
-   )
-   }
+    constructor(prop){
+        super(prop);
+        this.state = {
+            what : "the rubber duck",
+        }
+    }
+    
+    render() {
+        return (
+            <div className='App'>
+            <D3Bar  data = {[]} size={[500,200]} />
+            </div>
+        )
+    }
 }
 export default App
--- src/D3Bar.js
+++ src/D3Bar.js
@@ -2,59 +2,120 @@ import React, { Component } from 'react';
 import { scaleLinear } from 'd3-scale'
 import { max } from 'd3-array'
 import { select } from 'd3-selection'
+var url = "https://covidtracking.com/api/v1/states/tx/daily.json";
+
+
+
+    function status(response) {
+        if (response.status >= 200 && response.status < 300) {
+        return Promise.resolve(response)
+        } else {
+        return Promise.reject(new Error(response.statusText))
+        }
+    }
+    
+    function json(response) {
+        return response.json()
+    }
+
+        
 
 class D3Bar extends Component {
 
-    constructor() {
-      super();
-      this.inputRef = React.createRef();
-      this.node = React.createRef();
+    constructor(props) {
+      super(props)
+
+      this.inputRef = React.createRef()
+      this.node = React.createRef()
       this.createBarChart = this.createBarChart.bind(this)
+      this.state = {'data':[1,2,3,4,5] };
     }
+
+    // fetch the state daily json data
+    fetchData(){
+        fetch(url)
+        .then(status)
+        .then(json)
+        .then((data) => {
+          this.setState ({ 'data' : data })
+          this.createBarChart()
+          console.log('Request succeeded with JSON response', this.state);
+        }).catch(function(error) {
+          console.log('Request failed', error);
+        });
+    }
+
+    deaths()  {
+        // let d = this.state.data;
+        //let r = d.map(v => v.deaths ); 
+        //console.log("death data", this.state); 
+    }
+
     componentDidMount() {
-        this.createBarChart()
-     }
+        console.log("commponentDidMount()")
+        this.fetchData();
+        this.timer = setInterval(() => this.fetchData(), 5000);
+        this.createBarChart();  
+    }
+
+    componentWillUnmount() {
+        console.log("componentWillMount()")
+    }
+
      componentDidUpdate() {
+         console.log("componentDidUpdate()")
         this.createBarChart()
      }
+
      createBarChart() {
+        console.log("The Data Looks Like", this.state)
+        // don't render until the data arrives
+        if(this.state.data.length === 0) { return(null);}
+
+        let localData = (this.state.data.map(day=>day.deathIncrease));
+        console.log("Local Data", localData)
         const node = this.node
-        const dataMax = max(this.props.data)
+ 
+        const dataMax = max(localData)
+
         const yScale = scaleLinear()
            .domain([0, dataMax])
-           .range([0, this.props.size[1]])
+           .range([0, this.props.size[1]]);
+
      select(node)
         .selectAll('rect')
-        .data(this.props.data)
+        .data(localData)
         .enter()
         .append('rect')
      
      select(node)
         .selectAll('rect')
-        .data(this.props.data)
+        .data(localData)
         .exit()
         .remove()
      
      select(node)
         .selectAll('rect')
-        .data(this.props.data)
+        .data(localData)
         .style('fill', '#fe9922')
-        .attr('x', (d,i) => i * 25)
+        .attr('x', (d,i) => i * 4)
         .attr('y', d => this.props.size[1] - yScale(d))
         .attr('height', d => yScale(d))
-        .attr('width', 25)
+        .attr('width', 2)
      }  
     onClick() {
       this.inputRef.current.focus();
     }
   
     render() {
-      return (
+        // don't render until the data arrives
+        if (this.state.data.length === 0) return(null)
 
-        <svg ref= {n => {this.node = n}}
-            width={500} height={500}>
-        </svg>
-      );
+        return (
+            <svg ref = {n => {this.node = n}}
+                width={500} height={500}>
+            </svg>
+        );
     }
   }
   export default D3Bar
